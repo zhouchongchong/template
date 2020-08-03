c1075f230eb4dc25a6792ad09eadec5aca5e7411
zhenxu.guan <zhenxu45665>
2020-08-03 20:02:38 +0800

    sql保存接口

--- src/main/java/com/zhongruan/template/controller/IdentifierMappingInfoController.java
+++ src/main/java/com/zhongruan/template/controller/IdentifierMappingInfoController.java
@@ -1,19 +1,3 @@
-/**
- * @Copyright Beijing Jiangrongxin Technology Co,.Ltd 2020.
- * <p>
- * This material is the property of Beijing Jiangrongxin Technology Co,. Ltd.
- * and the information contained herein is confidential. This material,
- * either in whole or in part, must not be reproduced or disclosed to others
- * or used for purposes other than that for which it has been supplied without
- * Beijing Jiangrongxin's prior written permission,
- * or, if any part hereof is furnished by virtue of contract with a third party,
- * as expressly authorized under that contract.
- * <p>
- * *****************************************************************************
- * Date             Author      Version       Description
- * 2020/8/3         ${Author}  1.0.0         ${DESCRIPTION}
- * *****************************************************************************
- */
 package com.zhongruan.template.controller;
 
 import com.zhongruan.template.entity.IdentifierMappingInfo;
@@ -33,14 +17,14 @@ public class IdentifierMappingInfoController {
     private IdentifierMappingInfoService identifierMappingInfoService;
 
     //通过标识符id获取sql
-    @GetMapping("getById")
-    public ResultData getById(@RequestParam("id") int id){
+    @GetMapping("findById")
+    public ResultData findById(@RequestParam("id") int id){
         IdentifierMappingInfo identifierMappingInfo = identifierMappingInfoService.getById(id);
         return ResultData.success(identifierMappingInfo.getSqlContext());
     }
-
-/*    @PostMapping("insert"){
-        public ResultData insert(@RequestBody IdentifierMappingInfo ){
-
-    }*/
+        @PostMapping("/updateSql")
+        public ResultData updateSql(@RequestBody IdentifierMappingInfo identifierMappingInfo){
+            int i = identifierMappingInfoService.updateSql(identifierMappingInfo);
+            return ResultData.success(i);
+    }
 }
--- src/main/java/com/zhongruan/template/service/IdentifierMappingInfoService.java
+++ src/main/java/com/zhongruan/template/service/IdentifierMappingInfoService.java
@@ -1,19 +1,4 @@
-/**
- * @Copyright Beijing Jiangrongxin Technology Co,.Ltd 2020.
- * <p>
- * This material is the property of Beijing Jiangrongxin Technology Co,. Ltd.
- * and the information contained herein is confidential. This material,
- * either in whole or in part, must not be reproduced or disclosed to others
- * or used for purposes other than that for which it has been supplied without
- * Beijing Jiangrongxin's prior written permission,
- * or, if any part hereof is furnished by virtue of contract with a third party,
- * as expressly authorized under that contract.
- * <p>
- * *****************************************************************************
- * Date             Author      Version       Description
- * 2020/8/3         ${Author}  1.0.0         ${DESCRIPTION}
- * *****************************************************************************
- */
+
 package com.zhongruan.template.service;
 
 import com.zhongruan.template.dao.IdentifierMappingInfoMapper;
@@ -21,6 +6,7 @@ import com.zhongruan.template.entity.IdentifierMappingInfo;
 import com.zhongruan.template.entity.IdentifierMappingInfoExample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -43,5 +29,13 @@ public class IdentifierMappingInfoService {
                 identifierMappingInfo=identifierMappingInfos.get(0);
             }
        return identifierMappingInfo;
+    }
+
+    public int updateSql(IdentifierMappingInfo identifierMappingInfo) {
+        IdentifierMappingInfoExample identifierMappingInfoExample=new IdentifierMappingInfoExample();
+        IdentifierMappingInfoExample.Criteria criteria = identifierMappingInfoExample.createCriteria();
+        criteria.andIdEqualTo(identifierMappingInfo.getId());
+        int i = identifierMappingInfoMapper.updateByExampleSelective(identifierMappingInfo, identifierMappingInfoExample);
+        return i;
     }
 }
